6b475615848b55deb41fb94b1fbec36147184810
Daniil Nesterov <124503215+Cerlace@users.noreply.github.com>
2025-06-03 21:58:25 +0300

    queries for user

--- src/main/java/com/example/java_ifortex_test_task/service/UserService.java
+++ src/main/java/com/example/java_ifortex_test_task/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.java_ifortex_test_task.service;
 
 import com.example.java_ifortex_test_task.dto.UserResponseDTO;
+import com.example.java_ifortex_test_task.entity.DeviceType;
 import com.example.java_ifortex_test_task.mapper.UserMapper;
 import com.example.java_ifortex_test_task.repository.SessionRepository;
 import com.example.java_ifortex_test_task.repository.UserRepository;
@@ -8,21 +9,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-    private final SessionRepository sessionRepository;
     private final UserMapper userMapper;
 
     // Returns a User with the biggest amount of sessions
     public UserResponseDTO getUserWithMostSessions() {
-        return null;
+        return userMapper.toDto(userRepository.getUserWithMostSessions());
     }
 
     // Returns Users that have at least 1 Mobile session
     public List<UserResponseDTO> getUsersWithAtLeastOneMobileSession() {
-        return null;
+        return userRepository.getUsersWithAtLeastOneMobileSession(DeviceType.MOBILE).stream()
+                .map(userMapper::toDto)
+                .toList();
     }
 }
